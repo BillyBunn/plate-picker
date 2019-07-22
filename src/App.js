ae021cd31098ebc5643ce2f03d7682512602ba7e
Billy Bunn <williammbunn@gmail.com>
2019-07-22 11:56:24 -0700

    adds responsive sidebar

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
-import React, { useContext, useReducer } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import Header from './components/Header';
 import Page from './components/Page';
-import './styles.css';
-
-import { Routes } from './Context';
-
-import Drawer from './components/Drawer';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex'
-  }
-}));
 
 export default function App() {
-  const routes = useContext(Routes);
-  const classes = useStyles();
-
   return (
-    <Router>
-      <div className={classes.root}>
-        <CssBaseline />
-        <Header />
-        {routes.map(({ path, Component }) => (
-          <Route key={path} exact path={path}>
-          {/* if (path === '/') return <Drawer /> */}
-            {({ match }) => (
-              <CSSTransition
-                in={match != null}
-                timeout={300}
-                classNames="page"
-                unmountOnExit
-              >
-                <Page>
-                  <Drawer />
-                  <Component />
-                </Page>
-              </CSSTransition>
-            )}
-          </Route>
-        ))}
-      </div>
-    </Router>
+    <>
+      <CssBaseline />
+      <Page />
+    </>
   );
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,23 +1,116 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import Form from './Form';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3)
-  },
-  toolbar: theme.mixins.toolbar
+  }
 }));
 
-function Page({ children }) {
+function Page(props) {
+  const { container } = props;
   const classes = useStyles();
+  const theme = useTheme();
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen);
+  }
 
   return (
-    <Container maxWidth="md" className={`page ${classes.content}`}>
-      <div className={classes.toolbar} />
-      {children}
-    </Container>
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Hidden smUp implementation="css">
+        <Drawer
+          container={container}
+          variant="temporary"
+          anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          classes={{
+            paper: classes.drawerPaper
+          }}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+        >
+          <Form />
+          <div className={classes.toolbar} />
+          <Divider />
+        </Drawer>
+      </Hidden>
+      <Hidden xsDown implementation="css">
+        <Drawer
+          classes={{
+            paper: classes.drawerPaper
+          }}
+          variant="permanent"
+          open
+        >
+          <div className={classes.toolbar} />
+          <Divider />
+          <Form />
+        </Drawer>
+      </Hidden>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {props.children}
+      </main>
+    </div>
   );
 }
 
